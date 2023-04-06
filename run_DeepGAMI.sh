3d6cbda590092d1873080817d771533aad0c48ca
daifengwanglab <57010174+daifengwanglab@users.noreply.github.com>
2023-04-06 14:17:48 -0500

    Update and rename run_DeepGAMI_rosmap_new.sh to run_DeepGAMI.sh

---
+++ run_DeepGAMI.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python -u DeepGamiTrain.py --input_files "./demo/expMat_filtered.csv,./demo/efeature_filtered.csv" --disease_label_file "./demo/label_visual.csv" --num_fc_neurons '50' --latent_dim 100 --n_iter 100 --batch_size 30 --learn_rate 0.001 --out_reg 0.005 --corr_reg 1 --epochs 100 --cross_validate='True' --model_type='fully_connect' --save "." > "sc_MVC_result.txt"
+
--- run_DeepGAMI_rosmap_new.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ROSMAP braaksc
-# modality1: chipseq; modality2: gex
-
-
-python3 -u DeepGamiTrain.py --input_files="./../rosmap_GexEpi_noFS_latest/rosmap_GexEpi_chip_noFS_tr.csv,./../rosmap_GexEpi_noFS_latest/rosmap_GexEpi_gex_noFS_tr.csv" --intermediate_phenotype_files="./../rosmap_GexEpi_noFS_latest/rosmap_GexEpi_epl_adj_noFS_new.npz,./../rosmap_GexEpi_noFS_latest/rosmap_GexEpi_grn_adj_noFS_new.npz" --disease_label_file="./../rosmap_GexEpi_noFS_latest/rosmap_GexEpi_phen_noFS_tr.csv" --model_type='drop_connect' --cross_validate True --num_fc_neurons='350' --save "./deepgami_models/rosmap_cogdx_nfc350_split/" > "./results/deepgami_cogdx_nfc350_split.txt"
-
